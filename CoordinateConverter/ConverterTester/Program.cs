62a099683f4c91c858d20d449bf49c0c8ec23ddf
nyoung1 <nolen.young@wsu.edu>
2019-11-11 20:18:01 -0800

    added better commenting and a test file

---
+++ CoordinateConverter/ConverterTester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConverterTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CoordinateConverter.CoordinateConverter myConverter = new CoordinateConverter.CoordinateConverter();
+
+        }
+    }
+}
