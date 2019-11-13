7df5af5dfcd4d0fd1d93c1dc743f41ff2ca3a323
nyoung1 <nolen.young@wsu.edu>
2019-11-12 20:13:26 -0800

    bug fixing

--- CoordinateConverter/ConverterTester/Program.cs
+++ CoordinateConverter/ConverterTester/Program.cs
@@ -10,13 +10,21 @@ namespace CoordinateConverter
             GeoCoordinate myApartment = new GeoCoordinate(46.7302976970894, -117.168948054314);
             GeoCoordinate sloan = new GeoCoordinate(46.72937850, -117.16925380);
             MeterCoordinate sloanCoordinate = new MeterCoordinate();
-            MeterCoordinate myApartmentCoordinate = new MeterCoordinate();
+            MeterCoordinate myApartmentCoordinate = new MeterCoordinate(0,0);
 
             sloanCoordinate = myConverter.FindMeterCoordinateFromOrigin(myApartment, sloan);
 
             Console.WriteLine("!!!!!!!!!!Lat/Lon to Cartesian Coordinate Converter!!!!!!!!!!");
-            Console.WriteLine("Origin: 46.732518, -117.173337, (0,0)");
-            Console.WriteLine("Mystery Point: 46.730576, -117.168540, (?,?)");
+            Console.WriteLine("Result = (" + sloanCoordinate.X + "," + sloanCoordinate.Y + ")");
+
+            sloan = myConverter.MeterCoordtoGeoCoord(myApartment, myApartmentCoordinate, sloanCoordinate);
+
+            Console.WriteLine("!!!!!!!!!!OTHER WAY!!!!!!!!!!");
+            Console.WriteLine("Result = (" + sloan.Latitude + "," + sloan.Longitude + ")");
+
+            sloanCoordinate = myConverter.FindMeterCoordinateFromOrigin(myApartment, sloan);
+
+            Console.WriteLine("!!!!!!!!!!BACK AGAIN!!!!!!!!!!");
             Console.WriteLine("Result = (" + sloanCoordinate.X + "," + sloanCoordinate.Y + ")");
         }
     }
